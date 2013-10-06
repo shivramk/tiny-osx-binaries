2bc67be4a63a43d085d7f4712d8eff97ed1c7b3f
Shivram Khandeparker <shivramk@gmail.com>
2013-10-06 10:37:44 +0530

    Further changes to bring binary size down to 300 bytes
    Removed __PAGEZERO segment
    Removed __text section
    Changed the program to just return 42 as exit code

--- tiny3.asm
+++ tiny3.asm
@@ -1,5 +1,4 @@
 bits 64
-filesize equ 0x200
 ; mach_header_64
 
 _header:
@@ -7,7 +6,7 @@ _header:
     dd 0x01000007 ; cputype -> 0x01000000 (CPU_ARCH_ABI64) | 0x7 (CPU_TYPE_I386)
     dd 0x3        ; cpusubtype -> CPU_SUBTYPE_X86_64_ALL
 	dd 0x2        ; filetype -> MH_EXECUTE
-	dd 0x3        ; ncmds
+	dd 0x2        ; ncmds
 	dd cmdsize    ; sizeofcmds
 	dd 0x1        ; flags
     dd 0x0        ; reserved
@@ -16,22 +15,9 @@ cmdsize equ _load_end - _load_start
 
 _load_start:
 
-_load_cmd_1:
-	dd 0x19       ; cmd -> LC_SEGMENT_64
-	dd 0x48       ; cmdsize -> sizeof(segment_command_64)
-    db "__PAGEZERO",0,0,0,0,0,0 ; segname -> __PAGEZERO
-    dq 0x0        ; vmaddr
-    dq 0x1000     ; vmsize
-    dq 0x0        ; fileoff
-    dq 0x0        ; filesize
-    dd 0x0        ; maxprot
-    dd 0x0        ; initprot
-    dd 0x0        ; nsects
-    dd 0x0        ; flags
-
 _load_command_2:
 	dd 0x19       ; cmd -> LC_SEGMENT_64
-	dd 0x98       ; cmdsize
+	dd 0x48       ; cmdsize
     db "__TEXT",0,0,0,0,0,0,0,0,0,0 ; segname -> __TEXT
 	dq 0x1000     ; vmaddr
 	dq 0x1000     ; vmsize
@@ -39,22 +25,8 @@ _load_command_2:
 	dq filesize   ; filesize
 	dd 0x7        ; maxprot -> read, write, execute
 	dd 0x5        ; initprot -> VM_PROT_READ(0x1) | VM_PROT_EXECUTE(0x4)
-	dd 0x1        ; nsects
+	dd 0x0        ; nsects
     dd 0x0        ; flags
-
-_section_1:
-    db "__text",0,0,0,0,0,0,0,0,0,0 ; sectname -> __text
-    db "__TEXT",0,0,0,0,0,0,0,0,0,0 ; segname -> __TEXT
-    dq _program+0x1000 ; addr
-	dq proglen      ; size
-    dd _program     ; offset
-    dd 0x0          ; align
-    dd 0x0          ; reloff
-    dd 0x0          ; nreloc
-    dd 0x80000400   ; flags
-	dd 0x0          ; reserved1
-	dd 0x0          ; reserved2
-	dd 0x0          ; reserved3
 
 _load_command_6:
 	dd 0x05       ; cmd -> LC_UNIXTHREAD
@@ -66,21 +38,12 @@ times 16 dq 0x0   ; zero all general purpose registers r0 to r15
 times 4 dq 0x0    ; zero rflags, cs, fs, gs
 
 _load_end:
-times filesize-($-_header)-(_end-_program) db 0x0 ; pad to 4096 bytes
 
 _program:
-    mov rdi, 1
-    mov rsi, _hw+0x1000 ; program is loaded at 0x1000
-    mov rdx, len
-    mov eax, 0x2000004
+    mov rdi, 42
+    mov eax, 0x2000001 ; exit
     syscall
-
-    mov eax, 0x2000001
-    syscall
-    ret
-_hw:
-    db "hello world", 0xa
-
-len equ $-_hw
 _end:
+
 proglen equ _end-_program
+filesize equ _end-_header
